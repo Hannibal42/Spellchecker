15fe4b6c5f7781955587f0245b5a01dfe00b3b55
Simon Hanisch <hanischsimon@gmail.com>
2014-06-29 23:48:40 +0200

    started with optimizing my main functions

--- Spellchecker.hs
+++ Spellchecker.hs
@@ -1,10 +1,9 @@
 module Main where
+
 import Data.List (foldl')
 import Text.Read (readMaybe)
 import Data.List (sortBy)
 import Data.Ord (comparing)
-
-
 import System.Environment (getArgs)
 
 data Tree a = Node a Bool [Tree a] deriving Show
@@ -26,28 +25,28 @@ mainSpellcheck file1 file2 = do
        
     putStrLn "Generating Trie..."
     let trie = generateTree words2
-    print $ treeSize trie
     
+ 
     putStrLn "Trie generated!" 
     putStrLn "Starting correction..." 
     let sentences1 = sentences content1
     analyseSentence trie sentences1
-    print "Ende2"
+    print "Ende"
 
-analyseSentence :: [Tree Char] -> [(String,Char)] -> IO ()
+analyseSentence :: [Tree Char] -> [String] -> IO ()
 analyseSentence _    []     = print "Write down the sentence" -- Den Satz aufschreiben TODO
-analyseSentence trie ((cs,c):xs) = do 
+analyseSentence trie (cs:xs) = do 
     
     let words1 = words cs
-    mapM_ (analyseWord trie (cs,c)) words1
+    mapM_ (analyseWord trie cs) words1
     analyseSentence trie xs
 
-analyseWord :: [Tree Char] -> (String,Char) -> String -> IO ()
-analyseWord trie (cs,c) word
+analyseWord :: [Tree Char] -> String -> String -> IO ()
+analyseWord trie cs word
            | (contains trie word) = return ()
 	       | otherwise            = do
 	            print $ "Suggestions for " ++ word ++ " in the sentence:" 
-	            print $ cs ++ [c]  
+	            print $ cs 
 
 	            let resultList = (findSuggestions trie word)
 	            interface resultList 10
@@ -76,81 +75,141 @@ modResultList x resultList =  if (x>len) then makeIndex [0..len] (take len resul
 	                                 	makeIndex _     []          = []
 	                                 	makeIndex (y:ys) ((a,b):xs) = (a,y) : makeIndex ys xs
 
-analyseText :: [Tree Char] -> String -> IO () -- TODO: I need an interface!!!!!
-analyseText ts x = if (contains ts x) 
-	               then return ()
-	               else print $ "Suggestions for " ++ x ++ ": " ++ (foldl (\ string (word,minEdit) -> string ++ word ++ " " ++ (show minEdit) ++ ", ") "" (take 10 (findSuggestions ts x)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- findSuggestions trie "Halo" 
 findSuggestions :: [Tree Char] -> String -> [(String,Int)]
 findSuggestions ts word = sortBy (comparing snd) (calcSuggestions ts [] word 8 1 [0..(length word)])
 
+
+
 {- calcSuggestions trie [] "Halo" 8 1 [0..(length "Halo")]
 Parameters: Trie , the tempWord we build up while traversing the trie, the word we want suggestions for, the treshold for the minimum Editdistance, a accu, the row from the rood node-}
-calcSuggestions :: [Tree Char] -> String -> String -> Int -> Int -> [Int] -> [(String,Int)] -- TODO: Build a function that gives back True if the minEdit distance still can become better than the treshold, TODO: Delete the accu
-calcSuggestions []                _        _    _      _   _     = []
+calcSuggestions :: [Tree Char] -> String -> String -> Int -> Int -> [Int] -> [(String,Int)] 
+calcSuggestions []                _        _    _      _   _ = []
 calcSuggestions ((Node a b c):ts) tempWord word thresh acc lastrow
-               | b && (minEdit < thresh) =   -- word end and minimum Editdistance smaller than the threshold,
-                          (reverse (a:tempWord),minEdit) -- adding a new tupel to the result list,
-                          : (calcSuggestions ts tempWord word thresh acc lastrow) --searching through the remaining nodes on the same level,
-                          ++ (calcSuggestions c (a:tempWord) word thresh (acc+1) currow)  -- searching through the child nodes.
-               | otherwise = 
-                          (calcSuggestions ts tempWord word thresh acc lastrow) --searching through the remaining nodes on the same level,
-                          ++ (calcSuggestions c (a:tempWord) word thresh (acc+1) currow) -- searching through the child nodes.
-                            where
-                              row     = (calcRow a word [acc] lastrow)
-                              currow  = reverse row
-                              minEdit = head row
+               | b  && (bestEdit < thresh)  && (editDis < thresh)=                                              -- word end and minimum Editdistance smaller than the threshold,
+                             (reverse (a:tempWord),editDis)                                -- adding a new tupel to the result list,
+                          :  (calcSuggestions ts tempWord word thresh acc lastrow)         -- searching through the remaining nodes on the same level,
+                          ++ (calcSuggestions c (a:tempWord) word thresh (acc+1) currow)   -- searching through the child nodes.
+            
+               | b  && (editDis < thresh) && (editDis < thresh) = 
+                            (reverse (a:tempWord),editDis)                                 -- adding a new tupel to the result list,
+                          : (calcSuggestions ts tempWord word thresh acc lastrow)          -- searching through the remaining nodes on the same level,
+{-                | (bestEdit < thresh) =
+                            (calcSuggestions ts tempWord word thresh acc lastrow)          -- searching through the remaining nodes on the same level,
+                          ++ (calcSuggestions c (a:tempWord) word thresh (acc+1) currow)   -- searching through the child nodes.
+               
+-}
 
+               | otherwise = (calcSuggestions ts tempWord word thresh acc lastrow)        -- searching through the remaining nodes on the same level.
+                          ++ (calcSuggestions c (a:tempWord) word thresh (acc+1) currow)   -- searching through the child nodes.
+                            where
+                              row     = if (isSwap (a:tempWord) word) 
+                              	        then (calcRow a word [acc] lastrow)  --black magic
+                              	        else (calcRow a word [acc] lastrow)                --ordinary magic 
+
+                              currow  = reverse row
+                              editDis = head row                                       -- the minimmal edit distance at the moment
+                              bestEdit = editDis - ((length tempWord) - (length word)) -- the best minimal edit distance I still can achieve
+
+                              -- the first is my tempWord(its in reverse) the second is my word that needs correction, this function is "special"
+                              isSwap :: String -> String -> Bool
+                              isSwap [x]        _  = False
+                              isSwap (x1:x2:xs) ws = (x1 == y1) && (x2==y2)
+                                  where
+                       	            (y1,y2) = lastTwo ws
+                                    lastTwo :: String -> (Char,Char)
+                                    lastTwo []      = (' ',' ')  --there are no ' ' in seperated words so this evaluates to False
+                                    lastTwo [x]     = (' ',' ')
+                                    lastTwo [x1,x2] = (x1,x2)
+                                    lastTwo (x:xs)  = lastTwo xs
+
+
+
+                       	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{-calculates one row, takes the char of the node, the word we a correcting,
+the current row we are calculating and the previous row-}
 calcRow ::  Char -> [Char] -> [Int] -> [Int] -> [Int]
 calcRow _ []     cr     lastrow      = cr
 calcRow x (y:ys) (c:cr) (fir:sec:lr)
-                                   | (x==y)    = let ele = min fir (min (sec+1) (c+1)) in calcRow x ys (ele:c:cr) (sec:lr)
-                                   | otherwise = let ele = min (fir+1) (min (sec+1) (c+1)) in calcRow x ys (ele:c:cr) (sec:lr)  
+                  | (x==y)    = let ele = min fir (min (sec+1) (c+1))     --minimal element for the case sub=0,del=1,ins=1
+                                in calcRow x ys (ele:c:cr) (sec:lr)
+                  | otherwise = let ele = min (fir+1) (min (sec+1) (c+1)) -- minimal element for the case sub=1,del=1,ins=1
+                                in calcRow x ys (ele:c:cr) (sec:lr)  
 
-sentences :: String -> [(String,Char)]
+--seperates a text into its sentences 
+sentences :: String -> [String]
 sentences [] = []
 sentences cs = go cs []
         where
-        	go :: String -> String -> [(String,Char)]
-        	go []         [] = []
-        	go [c]        ws = if (isEnd c) then [(ws,c)] else [(ws ++ [c],' ')]
-        	go (c1:c2:cs) ws
-        	   | (isEnd c1) && (c2==' ') = (ws,c1) : (go cs []) 
-        	   | otherwise               = go (c2:cs) (ws ++ [c1]) 
+            isEnd c = (c=='.') || (c=='!') || (c=='?')     -- detects and end letter for sentence 
+            go :: String -> String -> [String]
+            go []         ws = []
+            go [c]        ws = [reverse (c:ws)]
+            go (c1:c2:cs) ws
+               | (isEnd c1) && (c2==' ') = (reverse (c1:ws)) : (go cs []) --end letter plus a following space is a sentence end
+               | otherwise               = go (c2:cs) (c1:ws) 
 
-isEnd c = (c=='.') || (c=='!') || (c=='?')     -- Ending of a sentence 
-
+--generates the trie with a wordlist
 generateTree :: [String] -> [Tree Char]
 generateTree ws = foldl' (insertInTree) [] ws
 
-insertInTree :: [Tree Char] -> String -> [Tree Char] -- main function to generate the trie 
-insertInTree []                [x]     = [(Node x True [])]   
-insertInTree []                (x:xs)  = [(Node x False (insertInTree [] xs))]  
-insertInTree ((Node a b c):ts) [x]     = if x /= a 
-	                                    then (Node a b c):(insertInTree ts [x])
-	                                    else (Node a True c):ts
-insertInTree ((Node a b c):ts) (x:xs)  = if x /= a 
-	                                    then (Node a b c):(insertInTree ts (x:xs))
-	                                    else (Node a b (insertInTree c xs)):ts
+-- takes a word and inserts it into a given trie 
+insertInTree :: [Tree Char] -> String -> [Tree Char] 
+insertInTree []                [x]     = [(Node x True [])]                     --creates a new end node for the last letter
+insertInTree []                (x:xs)  = [(Node x False (insertInTree [] xs))]  --creates a new child tree for the new letter
+insertInTree ((Node a b c):ts) [x]     = if x == a                              --case: the last letter of the word
+                                        then (Node a True c):ts                 --returns a end node for the letter and the remaining nodes on the level
+                                        else (Node a b c):(insertInTree ts [x]) --keeps searching for the right node
+insertInTree ((Node a b c):ts) (x:xs)  = if x == a                                 --case: a letter somewhere in the word
+                                        then (Node a b (insertInTree c xs)):ts     --found the correct node, going to the child nodes  
+                                        else (Node a b c):(insertInTree ts (x:xs)) --searches on the same level
 
-contains :: [Tree Char] -> String -> Bool   -- checks if a word is in a given trie 
+-- checks if a word is in a given trie 
+contains :: [Tree Char] -> String -> Bool   
 contains _  [] = True
 contains [] _  = False
-contains ((Node a b c):ts) [x]    = if a == x
-	                                then b
-	                                else contains ts [x]
-contains ((Node a b c):ts) (x:xs) = if a == x
-	                                then contains c  xs
-	                                else contains ts (x:xs)
-
--- Some Test for tree testing
-treeSize :: [Tree Char] -> Integer -- Returns the number of nodes in the tree
-treeSize []                = 0
-treeSize ((Node a b c):ts) = (treeSize c) + (treeSize ts) + 1
+contains ((Node a b c):ts) [x]    = if a == x             -- case: the last letter of the word
+                                    then b                -- returns the isEnd value of the last node we are looking at
+                                    else contains ts [x]  -- looks for other nodes on the same level that match our letter
+contains ((Node a b c):ts) (x:xs) = if a == x               -- case: a letter somewhere in the word
+                                    then contains c  xs     -- searches in the new child trees
+                                    else contains ts (x:xs) -- searches on the same level
 
 
-treeTest:: [String] -> [Tree Char] -> Bool   -- A short function to verify that all words that are in the wordlist are in the trie
-treeTest []     _  = True
-treeTest (x:xs) ts = (contains ts x) && (treeTest xs ts) 
+analyseText :: [Tree Char] -> String -> IO () -- TODO: I need an interface!!!!!
+analyseText ts x = if (contains ts x) 
+	               then return ()
+	               else print $ "Suggestions for " ++ x ++ ": " ++ (foldl (\ string (word,minEdit) -> string ++ word ++ " " ++ (show minEdit) ++ ", ") "" (take 10 (findSuggestions ts x)))
